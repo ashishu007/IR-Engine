646c6d733d5d5e54ba7a503f12d55f90fa6205a4
Ashish Upadhyay <panditu2015@gmail.com>
2020-09-19 15:23:24 +0100

    added new template

--- app/engine/inv_ind.py
+++ app/engine/inv_ind.py
@@ -2,10 +2,11 @@
 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-import os, collections, re
+import os, collections, re, pickle
 
 Stopwords = set(stopwords.words('english'))
 sws = list(Stopwords)
+# sws = ["a", "an", "the", "they", "i", "am", "you", "with", "on", "to", "was"]
 sws.append("doc")
 sws.append("docno")
 sws.append("document")
@@ -45,14 +46,19 @@ def get_ii(ftt, uw):
                 ii[word]["pos"].append({i: ctr[word]})
     return ii
 
-# def get_doc_names(file_dir):
-#     doc_names = {}
-#     for i, f in enumerate(os.listdir(file_dir)):
-#         doc_names[i] = f
-#     return doc_names
-
-def get_inv_ind():
-    file_to_terms, unique_words, file_content = process_files("./app/engine/data")
-    inv_ind = get_ii(file_to_terms, unique_words)
-    # print(file_to_terms)
+def get_inv_ind(load_saved=True):
+    if not os.path.exists("./app/engine/pkls/inverted_index.pkl"):
+        load_saved = False
+    if not load_saved:
+        print("loading files and creating index ...")
+        file_to_terms, unique_words, file_content = process_files("./app/engine/data")
+        print("files loaded, index created, now creating inverted index ...")
+        inv_ind = get_ii(file_to_terms, unique_words)
+        print("inverted ondex created ...")
+        pickle.dump(inv_ind, open("./app/engine/pkls/inverted_index.pkl", "wb"))
+        pickle.dump(file_content, open("./app/engine/pkls/file_content.pkl", "wb"))
+    else:
+        print("loading saved inverted_index")
+        inv_ind = pickle.load(open("./app/engine/pkls/inverted_index.pkl", "rb"))
+        file_content = pickle.load(open("./app/engine/pkls/file_content.pkl", "rb"))
     return inv_ind, file_content
--- app/routes.py
+++ app/routes.py
@@ -5,17 +5,14 @@ from app.engine.inv_ind import get_inv_ind
 # Show the input form
 @app.route('/')
 def hello_world():
-    # inv_ind = {"dog": [1, 2, 3], "cat": [2, 4]}
     inv_ind, docs = get_inv_ind()
-    # print(inv_ind)
-    # print(docs)
     return render_template("index.html", data=inv_ind, docs=docs)
 
 @app.route('/result', methods=["GET", "POST"])
 def send_result():
     if request.method == 'POST':
-        result = request.form
-        r = result.to_dict(flat=False)
-        # print(result)
-        print(r)
-    return jsonify(result)
+        form_result = request.form
+        form_r = form_result.to_dict(flat=False)
+        print(form_r)
+    # docs = retrieved_docs(form_r["query"])
+    return render_template("display.html", docs=form_r["query"])
